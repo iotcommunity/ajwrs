e78b9d2270a3dd7526719c6190d9504a050a922c
Eric Jordan <ejordan@ara.com>
2016-03-11 19:20:02 -0500

    Added comments to the code.

--- DayFour/src/hal_entry.c
+++ DayFour/src/hal_entry.c
@@ -2,11 +2,16 @@
 #include "hal_data.h"
 void hal_entry(void)
 {
+    // Create a simple message
     uint8_t data[] = "This is a test";
+
+    // Catch the function call results for debugging
     ssp_err_t openResult;
     ssp_err_t writeResult;
 
+    // Open the UART driver
     openResult = g_uart.p_api->open(g_uart.p_ctrl, g_uart.p_cfg);
+
+    // Send the message data
     writeResult = g_uart.p_api->write(g_uart.p_ctrl, data, 14);
-    ;
 }
